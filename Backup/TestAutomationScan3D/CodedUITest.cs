dc7e63c7929d039b7af5922fed293406a97956a8
unknown <caitanojunior@gmail.com>
2018-04-23 09:49:35 -0300

    create project test automation

---
+++ Backup/TestAutomationScan3D/CodedUITest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace TestAutomationScan3D
+{
+
+    [TestClass]
+    public class CodedUITest
+    {
+        private StackWhiteTest test;
+
+        [TestMethod]
+        public void RunTestAutomation()
+        {
+            this.StackWhiteTest.ClickCamera();
+
+        }
+
+        public StackWhiteTest StackWhiteTest
+        {
+            get
+            {
+                if ((this.test == null))
+                {
+                    this.test = new StackWhiteTest();
+                }
+                return this.test;
+            }
+        }
+    }
+}
---
+++ Backup/TestAutomationScan3D/StackWhiteTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WPFUIItems;
+using TestStack.White.WindowsAPI;
+
+namespace TestAutomationScan3D
+{
+    public class StackWhiteTest
+    {
+        //paths
+        private const string PATH_FOLDER = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\CloudCompare\\";
+
+        //variables
+        private static Application applicationCloudCompare;
+        private static Window mainWindow;
+
+
+        //Open CloudCompare.exe
+        private void CloudCompare(String action)
+        {
+
+            if (action == "open")
+            {
+                applicationCloudCompare = Application.Launch(PATH_FOLDER + "CloudCompare");
+                mainWindow = applicationCloudCompare.GetWindow(SearchCriteria.ByClassName("Qt5QWindowIcon"), TestStack.White.Factory.InitializeOption.WithCache);
+                mainWindow.WaitWhileBusy();
+
+            }
+            else if (action == "close")
+            {
+                applicationCloudCompare.Close();
+            }
+        }
+
+        public void ClickCamera()
+        {
+            CloudCompare("open");
+            //var buttonFile = mainWindow.Get<Button>(SearchCriteria.ByText("Camera settings"));
+            //buttonFile.Click();
+
+        }
+    }
+}
